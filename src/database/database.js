a79f27ca34237dc5bfdaf58ae8e1b3566b71b548
nyt23 <3121763@STUD.SRH-CAMPUS-BERLIN.DE>
2024-05-05 13:02:20 +0200

    modified saveData function

--- src/database/database.js
+++ src/database/database.js
@@ -1,14 +1,35 @@
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import * as path from "path";
 
-const readData = async (filename) => {
-    const filePath = path.join(__dirname, 'database', `${filename}.json`);
-    return fs.readFileSync(filePath);
-};
+export function readData(filename) {
+    // Construct the file path
+    const filePath = path.join(`./database/${filename}.json`);
 
-const saveData = async (filename, data) => {
-    const filePath = path.join(__dirname, 'database', `${filename}.json`);
-    return fs.writeFileSync(filePath, JSON.stringify(data), {flag: 'w'});
-};
+    // Attempt to read the file
+    try {
+        const data = fs.readFileSync(filePath, 'utf8'); // Ensure encoding is set to 'utf8' to get string output
+        return JSON.parse(data); // Parse the JSON string into a JavaScript object
+    } catch (error) {
+        console.error(`Failed to read or parse the file: ${filePath}`, error);
+        return null; // Return null or throw an error based on your error handling strategy
+    }
+}
 
-module.exports = { readData, saveData }
+export function saveData(filename, data) {
+    // Construct the file path
+    const filePath = path.join('database', `${filename}.json`);
+
+    try {
+        // Stringify the JSON data with added formatting for readability
+        const jsonData = JSON.stringify(data, null, 2);
+
+        // Write data to the file system
+        fs.writeFileSync(filePath, jsonData, { flag: 'w' });
+    } catch (error) {
+        console.error(`Failed to write data to ${filePath}`, error);
+        // Optionally throw the error to be handled by the caller
+        throw error;
+    }
+}
+
+//module.exports = { readData, saveData }
