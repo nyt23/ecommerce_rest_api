73f421136ca022f3fe8da7c33e5acdd4cf8db63a
nyt23 <3121763@STUD.SRH-CAMPUS-BERLIN.DE>
2024-05-05 20:18:57 +0200

    add PUT and DELETE endpoints

--- src/routers/users/userRouters.js
+++ src/routers/users/userRouters.js
@@ -7,8 +7,9 @@ import fs from 'fs';
 userRoutes.use(express.json());
 import {readData, saveData} from "../../database/database.js";
 import path from "path";
+import registerSchema from "../../schemas/userRegisterSchema.js";
 
-// POST api for registration
+// create users data
 userRoutes.post('/api/users', (req, res) => {
     //console.log(req.body);
 
@@ -57,15 +58,24 @@ userRoutes.post('/api/users', (req, res) => {
         createdAt: new Date().toISOString()
     }
 
-    users.push(newUser);
-    saveData('users', users)
-    //fs.writeFileSync('./database/users.json', JSON.stringify(users), {flag:'w'})
-    // res.set('Content-Type', 'text/plain');
-    res.send('Form data received successfully');
+    // validation
+    const {error, value} = registerSchema.validate(req.body);
+        if (error) {
+            res.status(400).send(JSON.stringify(error))
+            console.log('validation error: ', error.details);
+        } else {
+            console.log('validated data: ', value);
+            users.push(newUser);
+            saveData('users', users)
+            //fs.writeFileSync('./database/users.json', JSON.stringify(users), {flag:'w'})
+            // res.set('Content-Type', 'text/plain');
+            res.send('Form data received successfully');
+        }
     });
 });
 
 
+// get all users
 userRoutes.get('/api/users', (req, res) => {
     try {
         const userData = fs.readFileSync('./database/users.json')
@@ -78,6 +88,7 @@ userRoutes.get('/api/users', (req, res) => {
 });
 
 
+// get one user
 userRoutes.get('/api/users/:id', (req, res) => {
     try {
         // const userData = readData('users');
@@ -97,6 +108,73 @@ userRoutes.get('/api/users/:id', (req, res) => {
         res.status(500).send('Internal Server Error');
     }
 })
+
+
+// update one user
+userRoutes.put('/api/users/:id', (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+
+        // read the existing data in users.json
+        const usersData = fs.readFileSync('./database/users.json', 'utf-8');
+        const users = JSON.parse(usersData);
+
+        // find the user to update
+        const userId = req.params.id;
+        const userIndex = users.findIndex(user => user.id === userId);
+        if (userIndex === -1) {
+            return res.status(404).send('User Not Found');
+        }
+
+        // Hash the new password
+        const hashedPassword = bcrypt.hash(password, 10);
+
+        // update usersData
+        users[userIndex] = {
+            id: userId,
+            username: username || users[userIndex].username,
+            email: email || users[userIndex].email,
+            password: hashedPassword || users[userIndex].password,
+            createdAt: users[userIndex].createdAt,
+            updatedAt: new Date().toISOString()
+        }
+
+        // save the updated userData to file
+        fs.writeFileSync('./database/users.json', JSON.stringify(users), {flag:'w'});
+        res.status(200).send('User Updated Successfully!');
+    } catch (e) {
+        console.error(e);
+        res.status(500).send('Internal Server Error')
+    }
+});
+
+
+// delete one user
+userRoutes.delete('/api/users/:id', (req, res) => {
+    try {
+        const userData = fs.readFileSync('./database/users.json', 'utf-8');
+        let users = JSON.parse(userData);
+
+        // find the index of the user data to delete
+        const userId = req.params.id;
+        const userIndex = users.findIndex(user => user.id === userId);
+        if (userIndex === -1) {
+            return res.status(404).send('User Not Found');
+        }
+
+        // remove the user from the array
+        users.splice(userIndex, 1);
+
+        // save updated user array to file
+        fs.writeFileSync('./database/users.json', JSON.stringify(users));
+        res.status(201).send('User deleted successfully!');
+
+    } catch (e) {
+        console.error(e);
+        res.status(500).send('Internal Server Error');
+    }
+})
+
 
 
 export default userRoutes;
